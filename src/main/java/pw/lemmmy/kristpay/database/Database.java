91a58f068801b291fd2546bf7319146f716b7ae2
Lignum <me@lignum.pw>
2020-04-15 15:47:45 +0200

    Implement getTotalDistributedKrist

--- src/main/java/pw/lemmmy/kristpay/database/Database.java
+++ src/main/java/pw/lemmmy/kristpay/database/Database.java
@@ -6,9 +6,8 @@ import org.spongepowered.api.service.sql.SqlService;
 import pw.lemmmy.kristpay.KristPay;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.OptionalInt;
 
 public class Database {
 	private static final String DB_URI = KristPay.INSTANCE.getConfig().getDatabase().getConnectionURI();
@@ -86,5 +85,19 @@ public class Database {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public OptionalInt getTotalDistributedKrist() {
+		String balanceSum = "SELECT SUM(balance) FROM accounts";
+
+		try (Connection conn = dataSource.getConnection();
+			 Statement stmt = conn.createStatement()) {
+			ResultSet rs = stmt.executeQuery(balanceSum);
+			return OptionalInt.of(rs.getInt(0));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return OptionalInt.empty();
 	}
 }
